2747b5bfdfdafb33c4d5b2f389bd459da3737988
Phil Schanely <philschanely@gmail.com>
2018-04-07 18:02:22 -0400

    WIP scaffolding setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as firebase from "firebase";
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyD8-YFz1kDzJsqgMdLbEtzyVo_j8UeMG3Y",
+  authDomain: "photo-forms.firebaseapp.com",
+  databaseURL: "https://photo-forms.firebaseio.com",
+  projectId: "photo-forms",
+  storageBucket: "photo-forms.appspot.com",
+  messagingSenderId: "890222161817"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header>
+          <h1>Photo Forms</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <h2>Welcome!</h2>
+          <p>
+            This applicaiton is currently under development.
+            Check back again soon!
+          </p>
+          <ul className="tiles">
+            <li>
+              <img src="http://placehold.it/400x400x" alt="thing" />
+              <h3>Heading</h3>
+            </li>
+            <li>
+              <img src="http://placehold.it/400x400x" alt="thing" />
+              <h3>Heading</h3>
+            </li>
+            <li>
+              <img src="http://placehold.it/400x400x" alt="thing" />
+              <h3>Heading</h3>
+            </li>
+            <li>
+              <img src="http://placehold.it/400x400x" alt="thing" />
+              <h3>Heading</h3>
+            </li>
+          </ul>
+        </main>
       </div>
     );
   }
