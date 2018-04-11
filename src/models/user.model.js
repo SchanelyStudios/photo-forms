124c60c93e53a4905cfdc9daedcb1280f2fd84cf
Phil Schanely <philschanely@gmail.com>
2018-04-11 07:35:27 -0400

    v0.3.0
    Add basic authentication

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,39 @@
-export default class UserMdoel {
+import firebase from './firebase';
+
+export const uiAuthConfig = {
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  signInSuccessUrl: '/',
+  signInFlow: 'popup',
+};
+
+export class UserModel {
   _schema = {
     id: '',
     email: '',
     name: '',
     allowed: false
   };
+
+  static isAuthenticated() {
+    if (window.localStorage.getItem('PF_USER_UID')) {
+      return true;
+    }
+    return false;
+  }
+
+  static authenticate() {
+    return firebase.auth()
+  }
+
+  static getAuthenticated() {
+    return firebase.auth().onAuthStateChanged((user) => {
+      // TODO: Ensure user is on approved user list.
+      window.localStorage.setItem('PF_USER_UID', user.uid);
+      window.localStorage.setItem('PF_USER_NAME', user.name);
+      window.localStorage.setItem('PF_USER_EMAIL', user.email);
+      return user;
+    });
+  }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, Redirect } from 'react-router';
 import App from './components/App';
 import DashboardPage from './components/DashboardPage';
 import ViewFormPage from './components/ViewFormPage';
@@ -7,13 +7,29 @@ import ViewSubmissionPage from './components/ViewSubmissionPage';
 import EditFormPage from './components/EditFormPage';
 import EditSubmissionPage from './components/EditSubmissionPage';
 import SubmissionListPage from './components/SubmissionListPage';
+import LoginPage from './components/LoginPage';
+
+import { UserModel } from './models/user.model';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  let userIsAuthenticated = UserModel.isAuthenticated();
+  return (
+    <Route {...rest} render={(props) => (
+      userIsAuthenticated === true
+        ? <Component {...props} />
+        : <Redirect to="/login" />
+      )}
+    />
+  );
+};
 
 export default (
   <App>
-    <Route path="/" exact={true} component={DashboardPage} />
+    <PrivateRoute path="/" exact={true} component={DashboardPage} />
+    <Route path="/login" exact={true} component={LoginPage} />
     <Route path="/form/:formId" exact={true} component={ViewFormPage} />
-    <Route path="/form/:formId/edit" exact={true} component={EditFormPage} />
-    <Route path="/form/:formId/submissions" exact={true} component={SubmissionListPage} />
+    <PrivateRoute path="/form/:formId/edit" exact={true} component={EditFormPage} />
+    <PrivateRoute path="/form/:formId/submissions" exact={true} component={SubmissionListPage} />
     <Route path="/submission/:submissionId" exact={true} component={ViewSubmissionPage} />
     <Route path="/submission/:submissionId/edit" exact={true} component={EditSubmissionPage} />
   </App>
