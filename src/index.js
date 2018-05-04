8bb3ce4f9abb8e88563b2d5add67ef21354550bc
Phil Schanely <philschanely@gmail.com>
2018-05-04 16:27:48 -0400

    v1.0.5
    * Remove service worker and add deep linking .htaccess
    * Protect isFeatured value on save
    * Increment version

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import routes from './routes';
 import './styles.css';
-import registerServiceWorker from './registerServiceWorker';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(<BrowserRouter children={routes} basename={baseUrl} />, rootElement);
-registerServiceWorker();
