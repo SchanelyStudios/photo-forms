87f498a16c23e125c3c6cd46f8e7833296dc56fa
Phil Schanely <philschanely@gmail.com>
2018-06-12 19:49:24 -0400

    Add env variablles #52

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/auth';
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyD8-YFz1kDzJsqgMdLbEtzyVo_j8UeMG3Y",
-  authDomain: "photo-forms.firebaseapp.com",
-  databaseURL: "https://photo-forms.firebaseio.com",
-  projectId: "photo-forms",
-  storageBucket: "photo-forms.appspot.com",
-  messagingSenderId: "890222161817"
+  apiKey: proces.env.REACT_APP_PF_API_KEY,
+  authDomain: process.env.REACT_APP_PF_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_PF_DB_URL,
+  projectId: process.env.REACT_APP_PF_PROJECT_ID,
+  storageBucket: proces.env.REACT_APP_PF_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_PF_MSG_SENDER_ID
 };
 firebase.initializeApp(config);
 
