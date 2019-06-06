595df18d4510906290bb3428ee2af411e009e9c1
Phil Schanely <phil.schanely@dockyard.com>
2019-06-06 09:00:29 -0400

    Add markdown for formatting of text block.

--- src/components/common/Textblock.js
+++ src/components/common/Textblock.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 const Textblock = (props) => {
+  console.log(props);
   return (
     <div className="textblock" data-block-alias={props.alias}>
       <h3 className="textblock__heading">
         {props.heading}
       </h3>
-      {props.children}
+      <ReactMarkdown source={props.children} />
     </div>
   );
 }
